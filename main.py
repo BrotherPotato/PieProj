9248b3b4386ae3cb4003c8f581873c74d9bd4d0f
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-12-15 01:55:30 +0100

    Revert "s"
    This reverts commit 128906759e5e89b8e9e028211057f22d2eb26d07.

--- main.py
+++ main.py
@@ -3,16 +3,3 @@ import pygame
 WINDOW_HEIGHT = 840
 WINDOW_WIDTH = 800
 WINDOW_DIMENSIONS = WINDOW_WIDTH, WINDOW_HEIGHT
-
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode(WINDOW_DIMENSIONS)
-
-def play_game():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-
-        clock.tick(30)
-
-play_game()
