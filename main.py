8049f99c4faecfec24342325e37ddda6459b156d
PhilipRobertsson <phiro138@student.liu.se>
2022-12-15 02:37:12 +0100

    blue ball on white background

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
+pygame.init()
 
 WINDOW_HEIGHT = 840
 WINDOW_WIDTH = 800
-WINDOW_DIMENSIONS = WINDOW_WIDTH, WINDOW_HEIGHT
+WINDOW_DIMENSIONS = (WINDOW_WIDTH, WINDOW_HEIGHT)
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode(WINDOW_DIMENSIONS)
 
-def play_game():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+# Run until the user asks to quit
+running = True
+while running:
 
-        clock.tick(30)
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-play_game()
+    # Fill the background with white  
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    pygame.display.update()
+
+pygame.quit()
